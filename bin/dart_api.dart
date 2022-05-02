b0d402126b8eeb42bec1274e6b11999aa44cf235
Felipe Sales <soufeliposales@gmail.com>
2022-05-02 19:23:00 -0300

    working at json in dart

--- bin/dart_api.dart
+++ bin/dart_api.dart
@@ -1,5 +1,52 @@
-import 'package:dart_api/dart_api.dart' as dart_api;
+import 'dart:convert';
+import 'dart:developer' as developer;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_api.calculate()}!');
+  final cidadeJson = '''
+    [{ 
+      "id": 1,
+      "nome": "Uruburetama",
+      "regiao": {
+        "nome": "Nordeste"
+      }
+    },
+    { 
+      "id": 2,
+      "nome": "Tururu",
+      "regiao": {
+        "nome": "Nordeste"
+      }
+    }]
+  ''';
+
+  // [] -> List
+  // {} -> Map<String, dynamic>
+
+  final cidadeMap = json.decode(cidadeJson);
+
+  if (cidadeMap is List) {
+    print('éuma lista');
+  } else if (cidadeMap is Map) {
+    print('éuma map');
+  }
+
+  developer.log('${cidadeMap.runtimeType}');
+  developer.log('$cidadeMap');
+  developer.log('${cidadeMap[0].runtimeType}');
+
+  cidadeMap.forEach((city) {
+    developer.log('${city['nome']}');
+  });
+
+  cidadeMap.forEach((city) {
+    developer.log('${city['regiao']['nome']}');
+  });
+
+  final cidadeMapJson = {
+    'id': 1,
+    'nome': 'Uruburetama',
+    'uf': 'CE'
+  };
+  developer.log(json.encode(cidadeMapJson));
+  developer.log('${[json.encode(cidadeMapJson)]}');
 }
